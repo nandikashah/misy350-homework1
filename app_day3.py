ae8a49444b9c21c17723ece3f75c2fdd4841d83f
Nandika <nandika@udel.edu>
2026-02-27 11:15:21 -0500

    changes

---
+++ app_day3.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import time
+import json
+from pathlib import Path
+
+st.title("Course Management App")
+st.header("Assignments")
+st.subheader("Assignments Manager")
+
+next_assignment_id_number = 3
+
+#load assignments
+assignments = [
+    {
+        "id" : "HW1",
+        "title" : "Introduction to Database",
+        "description" : "bascis of database design",
+        "points" : 100,
+        "type" : "homework"
+    },
+    {
+        "id" : "HW2",
+        "title" : "Normalization",
+        "description" : "normalize the table designs",
+        "points" : 100,
+        "type" : "lab"
+    }
+]
+
+json_path = Path("assignments.json")
+
+if json_path.exists:
+    with json_path.open("r",encoding="utf=8") as f:
+        assignments = json.load(f)
+
+tab1, tab2, tab3 = st.tabs(["View Assignments", "Add New Assignment", "Update an Assignment"])
+
+with tab1:
+    #st.info("This tab is under development")
+    option = st.radio("View/Search",["View","Search"],horizontal = True)
+    if option == "View":
+        st.dataframe(assignments)
+
+    else:
+        search_title = st.selectbox("Assignment Titles", [])
+
+with tab2:
+    #add new assignment
+    st.markdown("### Add New Assignment")
+
+    #INPUT
+    title = st.text_input("Title",placeholder="ex: Homework 1",
+                        help="This is the name of the assignment")
+
+    #if you use "" instead of "Title", it'll remove the smaller grey title
+
+    description = st.text_area("Description",placeholder="ex: Database Design...")
+    due_date = st.date_input("Due Date")
+    assignments_type = st.radio("Type",["Homework","Lab"])
+
+    points = st.number_input("Points")
+
+
+    #assignments_type2 = st.selectbox("Type",["Homework","Lab","Other"])
+    #if assignments_type2 == "Other":
+    #   assignments_type2 = st.text_input("Assignment Type")
+
+    #lab = st.checkbox("Lab")
+
+    with st.expander("Assignment Preview"):
+        st.markdown("## Live Preview")
+        st.markdown(f"Title: {title}")
+
+    btn_save = st.button("Save", use_container_width=True, disabled=False)
+
+
+    if btn_save:
+        #st.warning("Working on it!")
+        with st.spinner("Saving the assignment..."):
+            time.sleep(5)
+            if not title:
+                st.warning("Enter Assignment Title")
+
+            else:
+                new_assignment_id = "HW" + str(next_assignment_id_number)
+                next_assignment_id_number += 1
+
+                assignments.append(
+                    {
+                        "id": new_assignment_id,
+                        "title": title,
+                        "description": description,
+                            "points": points,
+                            "type": assignments_type
+                    }
+                )
+                
+                #Recording the data into an actual file
+                #if json_path.exists():
+                with json_path.open("w",encoding="utf-8") as f:
+                    json.dump(assignments,f)
+
+
+                st.success("Assignment is recorded!")
+                st.dataframe(assignments)
+
+
+            st.success("Assignment is recorded!")
+
