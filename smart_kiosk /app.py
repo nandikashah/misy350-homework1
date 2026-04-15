8ffe3361630170ace358283b3185b1b6e46218b2
Nandika <nandika@udel.edu>
2026-04-15 11:45:53 -0400

    new stuff

--- smart_kiosk /app.py
+++ smart_kiosk /app.py
@@ -1,3 +1,27 @@
 import streamlit as st
+from data import data_manager
+from pathlib import Path
 
 st.set_page_config("Smart Kiosk")
+
+st.header("Smart Kiosk")
+
+if "inventory" not in st.session_state:
+    st.session_state["inventory"] = data_manager.load_data("smart_kiosk/inventory.json")
+
+if "orders" not in st.session_state:
+    st.session_state["orders"] = data_manager.load_data(Path("smart_kiosk/orders.json"))
+
+with st.sidebar:
+    st.markdown("## Smart Kiosk")
+
+tab1, tab2 = st.tabs(['Dashboard', 'Add New Order'])
+
+with tab1:
+    pass
+
+with tab2:
+    from ui import orders_ui
+    orders_ui.add_new_order_render()
+
+
--- smart_kiosk /services/kiosk_services.py
+++ smart_kiosk /services/kiosk_services.py
@@ -7,20 +7,35 @@ from typing import List, Dict, Optional
 
 #Query 4: find how many orders placed for the item using item id
 
-def place_order(inventory_items : list, item_id : str, quantity : int, orders : list):
+def place_order(inventory_items : list, item_id : str, quantity : int, orders : list) -> Optional[Dict]:
     #find item and check the existing inventory for the item
     item = find_inventory_item_by_item_id(inventory_items, item_id)
     if item:
-        if item['quantity'] >= quantity:
-            orders.append(
-                {
-                    "order_id" : str(uuid.uuid4())
-                }
-            )
+        if item['stock'] >= quantity:
+            item['stock'] = item['stock'] - quantity #reduce stock
+            total = quantity * item['unit_price']
+
+            #create new order dict
+            new_order = {
+                "order_id" : str(uuid.uuid4()),
+                "item_id" : item_id,
+                "quantity" : quantity,
+                "status" : "placed",
+                "total": total
+            }
+        #add the new order to the orders
+            
         # if we have enough inventory then reduce the inventory
         # then place the order
 
     pass
+
+def find_item_names(inventory: List) -> List:
+    item_names = []
+    for item in inventory:
+        item_names.append(item['name'])
+    return item_names
+
 
 def find_orders_by_item_id():
     pass
@@ -36,4 +51,13 @@ def find_inventory_item_by_item_id(inventory_list: list, item_id: str):
     return None
 
 def update_inventory_item():
+    pass
+
+def add_new_item_to_inventory():
+    pass
+
+def update_inventory_item():
+    pass
+
+def cancel_order():
     pass
